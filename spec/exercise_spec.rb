8f92b3803a13cc2f1bdc1ad9d7f99fd59a82b012
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-07-07 12:39:49 +0100

    returns ["3 * 4", 12.0] when given "3 * 4"

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -19,4 +19,7 @@ describe 'equationEvaluator' do
   it 'returns ["2 * 2", 4.0] when given "2 * 2"' do
     expect(equationEvaluator("2 * 2")).to eq ["2 * 2", 4.0]
   end
+  it 'returns ["3 * 4", 12.0] when given "3 * 4"' do
+    expect(equationEvaluator("3 * 4")).to eq ["3 * 4", 12.0]
+  end
 end
